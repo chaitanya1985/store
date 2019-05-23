722b7a824cd143427c92d944c5555549424a7888
chaitanya <chaitanya@gmail.com>
2019-05-23 13:30:03 +0530

    update devise

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,12 @@
 class WelcomeController < ApplicationController
+    before_action :authenticate_user!, :only => [:dashboard]
+
+    def index
+            
+    end
+    
+    def dashboard
+
+    end
+
 end
